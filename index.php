909b1f4783cef140abe75e6d299ac4c94af27978
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2014-10-27 19:06:19 +0200

    Shadow effect added

--- index.php
+++ index.php
@@ -3,36 +3,37 @@
 <head>
   <meta charset="utf-8"></meta>
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>Oh you building something new? How exciting!</title>
+  <title>Push button!</title>
   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="js/jquery.animate-shadow-min.js"></script>
   <script type="text/javascript" charset="utf-8" src="js/main.js"></script>
 </head>
 
 <body>
   <div class="container">
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: aquamarine;">
       </div>
     </div>
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: coral;">
       </div>
     </div>
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: lightGreen;">
       </div>
     </div>
     <div class="clearfix"></div>
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: tomato;">
       </div>
     </div>
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: gold;">
       </div>
     </div>
     <div class="btn">
-      <div class="btn-inner">
+      <div class="btn-inner" style="background-color: teal;">
       </div>
     </div>
   </div>
--- js/main.js
+++ js/main.js
@@ -1,41 +1,59 @@
 $(document).ready(function() {
   
   var upSize = 120;
-  var downSize = 70;
+  var downSize = 85;
   var upOffset = (upSize - 100) / 2;
   var downOffset = (100 - downSize) / 2;
+  var shadowDist = 4;
+  var shadowOffset = 1.8 * shadowDist;
 
-  $('.btn-inner').mouseover(function() {
 
-    $(this).animate({ width: upSize + '%', 
+  $('.btn-inner').css('box-shadow', shadowDist + 'px ' + shadowDist + 'px rgba(0, 0, 0, 0.5)')
+                 .mouseover(function() {
+
+    $(this).css('z-index', 600)
+           .animate({ width: upSize + '%', 
                       height: upSize + '%',
                       left: '-' + upOffset + '%',
-                      top: '-' + upOffset + '%'}, 100)
+                      top: '-' + upOffset + '%',
+                      boxShadow : shadowOffset + "px " + shadowOffset + "px "}, 
+                      100)
   })             
                  .mouseout(function() {
 
     $(this).animate({ width: '100%', 
                       height: '100%',
                       left: '0',
-                      top: '0'}, 80)
+                      top: '0',
+                      boxShadow : shadowDist + "px " + shadowDist + "px "},
+                      80, 
+                      function() {
+
+                        $(this).css('z-index', 500);
+                      })
   })
                  .mouseup(function() {
 
     $(this).animate({ width: '100%', 
                       height: '100%',
                       left: '0',
-                      top: '0'}, 80)
+                      top: '0',
+                      boxShadow : shadowDist + "px " + shadowDist + "px "},
+                      80,
+                      function() {
+
+                        $(this).css('z-index', 500);
+                      })
   })
                  .mousedown(function() {
 
     $(this).stop(true, true)
+           .css('z-index', 400)
            .animate({ width: downSize + '%', 
                       height: downSize + '%',
                       left: downOffset + '%',
-                      top: downOffset + '%'}, 50, function() {
-
-                        console.log('Hunting a bug, eh?');
-                      })
+                      top: downOffset + '%',
+                      boxShadow : "0px 0px 0px"}, 50);
   });
 
 });
